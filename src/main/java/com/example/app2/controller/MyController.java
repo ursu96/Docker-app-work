df6a2b0431e6cdbd4d7657a7df717c98ae74cccc
ursu96 <ursualexandru96@gmail.com>
2018-07-28 10:38:27 +0300

    Spring Profiles

--- src/main/java/com/example/app2/controller/MyController.java
+++ src/main/java/com/example/app2/controller/MyController.java
@@ -1,21 +1,26 @@
 package com.example.app2.controller;
 
+import com.example.app2.App2Application;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @Configuration
 @RestController
 public class MyController {
 
+    static Logger log = Logger.getLogger(App2Application.class.getName());
+
     @Value("${endpoint.welcome}")
     private String mesaj;
 
-
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String afiseazaMesajAppYML(){
+        log.warning(mesaj);
         return mesaj;
     }
 
